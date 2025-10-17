a73fa799f552c6b3dce69dae534ead8039fd9974
abhaycheruvadi <abhaycheruvadi@gmail.com>
2025-10-17 15:59:40 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Fitness Tracker',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF121212),
